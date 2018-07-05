b916ae857f82f8c745c9c8abe6e38950f91c0f98
Brice Fernandes <brice@weave.works>
2018-07-05 18:31:10 +0100

    Feature: Create priority + and - actions

---
+++ cmd/priMinus.go
@@ -0,0 +1,28 @@
+package cmd
+
+import (
+	"strconv"
+
+	"github.com/hypotheticalco/tracker-client/todo"
+	"github.com/hypotheticalco/tracker-client/utils"
+	"github.com/spf13/cobra"
+)
+
+var priMinus = &cobra.Command{
+	Use:   "pri- [item number]",
+	Short: "Decrease the priority for the selected task",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+
+		id, err := strconv.Atoi(args[0])
+		utils.DieOnError("Supplied argument '"+args[0]+"' was not an integer: ", err)
+		t := todo.GetTodoID(id)
+
+		todo.Replace(id, t.DecreasePriority())
+
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(priMinus)
+}

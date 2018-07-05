f5e516bd24e3440fb2447a1fd6581ae267ac2c47
Brice Fernandes <brice@weave.works>
2018-07-05 12:43:33 +0100

    Make default action to show the current todo list

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"github.com/hypotheticalco/tracker-client/todo"
 	"github.com/spf13/cobra"
 )
 
@@ -10,7 +11,8 @@ var RootCmd = &cobra.Command{
 	Short: "Hypothetical Tasks & Time Tracker is a todo list manager and time tracker",
 	Run: func(cmd *cobra.Command, args []string) {
 		println(cmd.Short)
-		println()
-		cmd.Usage()
+		println("For help and usage, use -h, --help or help")
+		//cmd.Usage()
+		todo.Show(todo.GetCurrentContext(), []string{})
 	},
 }

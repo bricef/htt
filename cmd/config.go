c65bb8cdd6115ec07e3aef14a9cf6c989914b4ef
Brice Fernandes <brice@weave.works>
2018-07-09 20:29:15 +0100

    Feature: Allow printing the config in YAML

---
+++ cmd/config.go
@@ -0,0 +1,26 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/hypotheticalco/tracker-client/utils"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	yaml "gopkg.in/yaml.v2"
+)
+
+var config = &cobra.Command{
+	Use:   "config",
+	Short: "Prints out the current configuration in YAML",
+	Args:  cobra.NoArgs,
+	Run: func(cmd *cobra.Command, args []string) {
+		c := viper.AllSettings()
+		bs, err := yaml.Marshal(c)
+		utils.DieOnError("Could not get YAML config. ", err)
+		fmt.Print(string(bs))
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(config)
+}
